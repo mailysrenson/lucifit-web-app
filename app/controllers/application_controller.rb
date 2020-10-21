96942a8cca927839094c3e506f48950e0397d640
mailys renson <mailys_94@hotmail.com>
2020-10-21 20:18:22 +0200

    small changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,17 @@ class ApplicationController < ActionController::Base
     before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
     protect_from_forgery with: :exception
+    before_action :set_locale
 
     def respond_modal_with(*args, &blk)
       options = args.extract_options!
       options[:responder] = ModalResponder
       respond_with *args, options, &blk
+    end
+
+
+    def set_locale
+      I18n.locale = I18n.default_locale
     end
 
     protected 
